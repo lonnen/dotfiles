b3dd7020a6a82217822680bcd5d063182074f100
lonnen <chris.lonnen@gmail.com>
2013-05-02 14:26:22 -0700

    fix install bugs: fail harder, use lonnen's repo
    fixes several installation bugs:
    * install from lonnen's repo instead of cowboy's
    * never try to install homebrew, ruby, or other deps
    * instead, fail loudly if the deps are missing

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -1,19 +1,6 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-# Some tools look for XCode, even though they don't need it.
-# https://github.com/joyent/node/issues/3681
-# https://github.com/mxcl/homebrew/issues/10245
-if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
-  sudo xcode-select -switch /usr/bin
-fi
-
-# Install Homebrew.
-if [[ ! "$(type -P brew)" ]]; then
-  e_header "Installing Homebrew"
-  true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
-fi
-
 if [[ "$(type -P brew)" ]]; then
   e_header "Updating Homebrew"
   brew update
