989dfa31045c6115c75faf1b3579cf5cbcad2dde
lonnen <chris.lonnen@gmail.com>
2013-05-31 11:17:57 -0700

    add virtual env to prompt, change toplevel prompt
    replaced a bunch of brackets with whitespace and introduced a second color

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -5,7 +5,7 @@
 # http://benalman.com/about/license/
 #
 # Example:
-# [lonnen@musashi:~/.dotfiles][master:!?]
+# (socorro)lonnen@musashi:~/.dotfiles master:!?
 # [11:14:45] $
 
 # ANSI CODES - SEPARATE MULTIPLE VALUES WITH ;
@@ -23,6 +23,7 @@ if [[ ! "${prompt_colors[@]}" ]]; then
   prompt_colors=(
     "36" # information color
     "37" # bracket color
+    "34" # secondary info color
     "31" # error color
   )
 
@@ -61,9 +62,9 @@ function prompt_git() {
       END {print r}'
   )"
   if [[ "$flags" ]]; then
-    output="$output$c1:$c0$flags"
+    output="$output$c1:$c2$flags"
   fi
-  echo "$c1[$c0$output$c1]$c9"
+  echo " $c1$c2$output$c9"
 }
 
 # SVN info.
@@ -74,7 +75,17 @@ function prompt_svn() {
   if [[ "$info" ]]; then
     last="$(echo "$info" | awk '/Last Changed Rev:/ {print $4}')"
     current="$(echo "$info" | awk '/Revision:/ {print $2}')"
-    echo "$c1[$c0$last$c1:$c0$current$c1]$c9"
+    echo "  $c2$last$c1:$c2$current$c9"
+  fi
+}
+
+# virtual environment info
+function prompt_venv() {
+  prompt_getcolors
+  [[ "$VIRTUAL_ENV" ]] || return;
+  local info="$(basename $VIRTUAL_ENV)"
+  if [[ "$info" ]]; then
+    echo "$c1($c2$info$c1)$c9"
   fi
 }
 
@@ -98,13 +109,15 @@ function prompt_command() {
   prompt_getcolors
   # http://twitter.com/cowboy/status/150254030654939137
   PS1="\n"
+  # virtual env: (venv)
+  PS1="$PS1$(prompt_venv)"
   # misc: [cmd#:hist#]
   # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
-  # path: [user@host:path]
-  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
-  # svn: [repo:lastchanged]
+  # path: user@host:path
+  PS1="$PS1$c1$c0\u$c1@$c0\h$c1:$c0\w$c1$c9"
+  # svn: repo:lastchanged
   PS1="$PS1$(prompt_svn)"
-  # git: [branch:flags]
+  # git: branch:flags
   PS1="$PS1$(prompt_git)"
   PS1="$PS1\n"
   # date: [HH:MM:SS]
