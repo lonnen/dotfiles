3a6921e7c7040d8aa73011725260bc01d2d742aa
lonnen <chris.lonnen@gmail.com>
2014-04-21 17:16:08 -0700

    switch to atom

--- init/20_sublime.sh
+++
@@ -1,4 +0,0 @@
-# set up package control
-ln -s ~/.dotfiles/conf/st2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text 2/Installed\ Packages
-ln -s ~/Dropbox/appdata/sublime/Packages ~/Library/Application\ Support/Sublime\ Text 2/Packages
-ln -s ~/Dropbox/appdata/sublime/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text 2/Pristine\ Packages
