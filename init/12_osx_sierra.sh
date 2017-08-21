e172399fec618fa7f8ebefcf054c7b099514abe6
lonnen <chris.lonnen@gmail.com>
2017-08-21 14:16:33 -0700

    prevent key destruction

--- init/12_osx_sierra.sh
+++ init/12_osx_sierra.sh
@@ -1,13 +1,10 @@
 # only built 4 MacOS Sierra
 is_osx 12 || return 1
 
-# power off memory in hibernation
-# docs.hardentheworld.org/OS/MacOS_10.12_Sierra/index.html
-# + some settings to avoid power naps
+# preset some power management stuff
 sudo pmset -a darkwakes 0
 sudo pmset -a standby 0
 sudo pmset -a standbydelay 0
-sudo pmset -a destroyfvkeyonstandby 1 hibernatemode 25
 
 # disable creation of metadata files
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
