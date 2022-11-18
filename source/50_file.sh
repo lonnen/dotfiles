2b3d253831841dbf27165686625ea1fc3a3add06
lonnen <chris.lonnen@gmail.com>
2022-11-18 00:31:49 -0800

    trim unused aliases

--- source/50_file.sh
+++ source/50_file.sh
@@ -1,23 +1,5 @@
 # Always use color output for `ls`
 alias ls="command ls -G"
 
-# Directory listing
-alias ll='ls -al'
-alias lsd='CLICOLOR_FORCE=1 ll | grep --color=never "^d"'
-
-# Easier navigation: .., ..., -
-alias ..='cd ..'
-alias ...='cd ../..'
-alias -- -='cd -'
-
-# File size
-alias fs="stat -f '%z bytes'"
-alias df="df -h"
-
 # Recursively delete `.DS_Store` files
 alias dsstore="find . -name '*.DS_Store' -type f -ls -delete"
-
-# Create a new directory and enter it
-function md() {
-  mkdir -p "$@" && cd "$@"
-}
