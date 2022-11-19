95c819e837c9cd9d0fec1f5900f650e7e33b1196
lonnen <chris.lonnen@gmail.com>
2022-11-19 01:40:52 -0800

    consolidate python

--- source/12_python.sh
+++ source/12_python.sh
@@ -1,3 +1,9 @@
+### PyEnv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+### virtualenvwrapper
 if [ "$(type -P virtualenvwrapper.sh)" ]; then
   export WORKON_HOME=$HOME/.virtualenvs
   export PROJECT_HOME=$HOME/Devel
