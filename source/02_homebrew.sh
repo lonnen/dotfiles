0579c8146498172c1f3a1764baea2e8af93be518
lonnen <chris.lonnen@gmail.com>
2023-09-24 22:21:16 -0700

    add homebrew's sbin to the path

--- source/02_homebrew.sh
+++ source/02_homebrew.sh
@@ -3,3 +3,6 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_GITHUB_API=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
+
+# Get homebrew's sbin on the path
+export PATH="/usr/local/sbin:$PATH"
