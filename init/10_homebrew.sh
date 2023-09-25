c54f4e0315999f6241dddd9490fb8e45d7981ef6
lonnen <chris.lonnen@gmail.com>
2023-09-24 22:30:00 -0700

    clarify the homebrew init

--- init/10_homebrew.sh
+++ init/10_homebrew.sh
@@ -1,22 +1,25 @@
+# Get the apropriate homebrew settings in the environment
 source $DOTFILES/source/02_homebrew.sh
 
-# Homebrew wants sbin
+# Homebrew wants sbin to exist
 if [ ! -d "/usr/local/sbin" ]; then
   sudo mkdir -p /usr/local/sbin
   sudo chown -R $(whoami) /usr/local/sbin
 fi
 
-# Install Homebrew.
+# Install Homebrew
 if [[ ! "$(type -p brew)" ]]; then
   e_header "Installing Homebrew"
   true | "/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""
 fi
 
-# Exit if, for some reason, Homebrew is not installed.
+# Exit if, for some reason, Homebrew is not installed
 [[ ! "$(type -p brew)" ]] && e_error "Homebrew failed to install." && return 1
 
 e_header "Updating Homebrew"
 brew doctor
 brew update
 brew install mas # ensure mac app store CLI is installed before we read the
+
+# install everything from the Brewfile
 brew bundle --file=$DOTFILES/conf/Brewfile
