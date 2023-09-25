3508bd3831dde04a2ef7b500b3ccc31eda841e45
lonnen <chris.lonnen@gmail.com>
2023-09-24 22:14:48 -0700

    zsh uses lowercast p as an argument for type

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -15,13 +15,13 @@ if [ ! -d "/usr/local/sbin" ]; then
 fi
 
 # Install Homebrew.
-if [[ ! "$(type -P brew)" ]]; then
+if [[ ! "$(type -p brew)" ]]; then
   e_header "Installing Homebrew"
   true | "/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""
 fi
 
 # Exit if, for some reason, Homebrew is not installed.
-[[ ! "$(type -P brew)" ]] && e_error "Homebrew failed to install." && return 1
+[[ ! "$(type -p brew)" ]] && e_error "Homebrew failed to install." && return 1
 
 e_header "Updating Homebrew"
 brew doctor
