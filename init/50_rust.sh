2bd22ca9cde45fb40e2a0b11da6df54d3e7bea62
lonnen <chris.lonnen@gmail.com>
2023-09-24 22:31:00 -0700

    clarify rust initialization

--- init/50_rust.sh
+++ init/50_rust.sh
@@ -1,4 +1,6 @@
-if [[ ! "$(type -P cargo)" ]]; then
+e_header "Initializing Rust from rustup"
+
+if [[ ! "$(type -p cargo)" ]]; then
   e_header "Installing Rust"
   true | curl https://sh.rustup.rs -sSf | sh -s -- -y
 fi
