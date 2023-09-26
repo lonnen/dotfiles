eb1b6e354f800e930a06b00498f438e7031fbc57
lonnen <chris.lonnen@gmail.com>
2023-09-26 01:14:22 -0700

    replace date formatting with zsh date formatting

--- source/01_prompt.sh
+++ source/01_prompt.sh
@@ -165,7 +165,7 @@ function __prompt_command() {
   fi
   PS1=$PS1$'\n'
   # date: [HH:MM:SS]
-  #PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+  PS1="$PS1$c1%D{[$c0%K$c1:$c0%M$c1:$c0%S$c1]}$c9"
   # exit code: 127
   PS1="$PS1$c2%(?..%?)$c9"
   PS1="$PS1 \$ "
