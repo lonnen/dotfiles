31390501af7f70aa3ea4604e320b142934cb0cd0
lonnen <chris.lonnen@gmail.com>
2023-10-12 16:59:04 -0700

    let homebrew export what it needs

--- source/02_homebrew.sh
+++ source/02_homebrew.sh
@@ -4,8 +4,9 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_GITHUB_API=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 
-# Get homebrew's sbin on the path
-export PATH="/usr/local/sbin:$PATH"
-
 # Homebrew Brewfile
 export HOMEBREW_BREWFILE=~/.brewfile
+
+# Let homebrew export what it needs to function well
+# note: this line implicity messes with the PATH
+eval "$(brew shellenv)"
