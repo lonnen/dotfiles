a9941b2b52f9eba90ff58097224e7e1c47b74357
lonnen <chris.lonnen@gmail.com>
2023-10-12 23:09:13 -0700

    call brew by absolute path in order to bootstrap the environment

--- source/02_homebrew.sh
+++ source/02_homebrew.sh
@@ -8,5 +8,13 @@ export HOMEBREW_NO_INSECURE_REDIRECT=1
 export HOMEBREW_BREWFILE=~/.brewfile
 
 # Let homebrew export what it needs to function well
-# note: this line implicity messes with the PATH
-eval "$(brew shellenv)"
+# this implicity messes with the PATH
+# location of brew varies by hardware
+# see: https://docs.brew.sh/FAQ#why-is-the-default-installation-prefix-opthomebrew-on-apple-silicon
+if [[ "$(uname -m)" == "arm64" ]]; then
+  # Apple Silicon
+  eval "$(/opt/homebrew/brew shellenv)"
+else
+  # Intel
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
