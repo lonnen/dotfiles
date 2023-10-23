70200d89254709b24040be1abe1509acf16e83a7
lonnen <chris.lonnen@gmail.com>
2023-10-22 20:07:10 -0700

    update first-run reminders

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -1,33 +1,45 @@
 # This file is sourced at the end of a first-time dotfiles install.
-setopt ALIASES
-source ~/.zshrc
 
 cat <<EOF
 Remember!
 * Sync your private dotifles
-* Manually load all your app store purchases
 * Sign into all your apps and sync everything those can sync
-* scp -r old_machine:~/* new_machine:~/
+* $ scp -r old_machine:~/* new_machine:~/
 * be careful with ~/Library/Preferences/com.apple.*
-** cp -r ~/Library/Preferences ~/Library/Preferences-Backup
-** cp -ir ~/Desktoper/Prefs/* ~/Library/Preferences/
+  $ cp -r ~/Library/Preferences ~/Library/Preferences-Backup
+  $ cp -ir ~/Desktoper/Prefs/* ~/Library/Preferences/
 EOF
 
-# docs.hardentheworld.org/OS/MacOS_10.12_Sierra/index.html
 cat <<EOF
 Check the following:
-  * Finder ⇒ Preferences ⇒ Advanced
-  * Finder ⇒ Preferences ⇒ Sidebar
-  * System Preferences ⇒ General -> Handoff
-  * System Preferences ⇒ General -> Recent Items
-  * System Preferences ⇒ Security & Privacy ⇒ Advanced -> Require Admin
-  * System Preferences ⇒ Security & Privacy ⇒ FileVault
-  * System Preferences ⇒ Security & Privacy ⇒ Firewall
-  * System Preferences ⇒ Security & Privacy ⇒ General -> Require Pass
-  * System Preferences ⇒ Security & Privacy ⇒ Privacy
-  * System Preferences ⇒ Security & Privacy ⇒ Privacy ⇒ Diagnostics & Usage
-  * System Preferences ⇒ Security & Privacy ⇒ Privacy ⇒ Location Services
-  * System Preferences ⇒ Spotlight
-  * System Preferences ⇒ Users & Groups ⇒ Guest User
-  * System Preferences ⇒ Users & Groups ⇒ Login Options -> Password Hints
+  * Finder ⇒ Preferences ⇒ Advanced ⇒ Show all Filename extensions
+  * Finder ⇒ Preferences ⇒ Sidebar ⇒ Configure your Favorites
+  * System Preferences ⇒ Printers & Scanners ⇒ Add your Printer
+  * System Preferences ⇒ Trackpad
+    ⇒ Point & Click ⇒ Disable "Look up & data detectors"
+    ⇒ Scroll & Zoom ⇒ Disable "Natural Scrolling", "Smart Zoom"
+    ⇒ More Gestures ⇒ Disable "Scroll between pages", "Swipe between full-screen applications"
+  * System Preferences ⇒ Lock Screen
+    ⇒ set "Start Screen Saver when inactive", "Turn display off *", to "Never"
+    ⇒ set "Require password after screen saver begins..." to "after 5 seconds"
+    ⇒ disable "show password hints"
+  * System Preferences ⇒ Screen Saver ⇒ Disable All
+  * System Preferences ⇒ Wallpaper ⇒ Set Custom Color to "#191919"
+  * System Preferences ⇒ Displays ⇒ Set resolution to "More Space"
+  * System Preferences ⇒ Desktop & Dock
+    ⇒ Adjust dock size and magnification (both low)
+    ⇒ Position on Screen to "right"
+    ⇒ Automatically hide and show the Dock to "disabled"
+    ⇒ Animate opening applications to "disabled"
+    ⇒ Show suggested and recent apps in Dock to "disabled"
+    ⇒ Disable all thigns related to Stage Manager
+    ⇒ Disable Widgets
+    ⇒ Default web browser to "Firefox"
+    ⇒ Prefer tabs when opening documents to "Never"
+    ⇒ Ask to keep changes when closing documents to "enabled"
+    ⇒ Close windows when quitting an application to "disabled"
+  * System Preferences ⇒ Sound ⇒ Alert Sound to "Funky"
+  * System Preferences ⇒ Keyboard
+    ⇒ Spelling and prediction ⇒ Disable Inline Predictive Text
+    ⇒ Modifier Keys ⇒ "Caps Lock key" to "Option"
 EOF
