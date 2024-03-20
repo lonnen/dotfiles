f3f2b477dee72fb09746093b7d6c195e910df562
lonnen <chris.lonnen@gmail.com>
2024-03-19 23:46:10 -0700

    adopt idiomatic zsh test across init scripts

--- init/50_rust.sh
+++ init/50_rust.sh
@@ -1,6 +1,5 @@
 e_header "Initializing Rust from rustup"
-
-if [[ ! "$(type -p cargo)" ]]; then
+if ! (( $+commands[cargo] )); then
   e_header "Installing Rust"
   true | curl https://sh.rustup.rs -sSf | sh -s -- -y
 fi
